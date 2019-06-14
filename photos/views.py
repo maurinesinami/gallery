8ce5a05901f66eac0d0ec062795721ae836ee342
maurinenami <maurinenami>
2019-06-14 11:28:00 +0300

    creayed the base html and created an if statement for the title

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def welcome(request):
-    return HttpResponse('welcome to my gallery')
+    return render(request,'welcome.html')
 
