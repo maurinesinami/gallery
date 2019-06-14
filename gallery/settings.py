1f6ea7559318339697b3a2b742f9b919d3964298
maurinenami <maurinenami>
2019-06-14 12:19:39 +0300

    created a new template navbar.html

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'photos.apps.PhotosConfig',
-    ' bootstrap3'
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
