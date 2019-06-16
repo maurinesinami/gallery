5084d0e0c8ad7b88baf6b6efb95ff9bd0847d2f9
maurinenami <maurinenami>
2019-06-16 20:06:03 +0300

    changed the database to postgres and the timezone to africa nairobi

--- gallery/settings.py
+++ gallery/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos',
+        'USER': 'maurine',
+        'PASSWORD':'maurinesinami',
+    }
     }
 }
 
@@ -107,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairob'
 
 USE_I18N = True
 
