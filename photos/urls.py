df3aca77c786b30e1fa8b5e011614325ba8b8b9d
maurinenami <maurinenami>
2019-06-16 20:40:31 +0300

    added a search form in the navbar and also added the router link inside urls.py

--- photos/urls.py
+++ photos/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns=[
-    url('^$',views.welcome,name = 'home-page')
+    url('^$',views.welcome,name = 'home-page'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
