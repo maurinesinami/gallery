9473276920451df71ee0b35e339effa7c344d286
maurinenami <maurinenami>
2019-06-16 20:52:27 +0300

    adde the search results function in views.py

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField()
+    image_path = models.ImageField(upload_to = 'gallery/')
+    def __str__(self):
+        return self.image_name
 
-# Create your models here.
+    @classmethod
+    def search_by_category(cls,search_term):
+        search_result = cls.objects.filter(image_category__cat_name__icontains=search_term)
+        return search_result
+
+    @classmethod
+    def get_image_by_id(cls,incoming_id):
+        image_result = cls.objects.get(id=incoming_id)
+        return image_result
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+        
+    @classmethod
+    def retrieve_all(cls):
+        all_objects = Image.objects.all()
+        for item in all_objects:
+            return item
+
+
+    @classmethod
+    def update_image(cls,current_value,new_value):
+        fetched_object = Image.objects.filter(image_name=current_value).update(image_name=new_value)
+        return fetched_object
+
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def welcome(request):
-    return render(request,'index.html')
+    all_images = Image.objects.all()
+    return render(request,'index.html',{'all_images':all_images})
+def search_results(request):
+    
+    if 'searchItem' in request.GET and request.GET["searchItem"]:
+        search_term = request.GET.get("searchItem")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'index.html',{"message":message,"all_images": searched_image})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'index.html',{"message":message})
 
