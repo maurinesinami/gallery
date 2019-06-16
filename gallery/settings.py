bd3929cfbc33d9c687c048460b2c1692af031c5d
maurinenami <maurinenami>
2019-06-16 20:08:20 +0300

    did the first migration

--- gallery/settings.py
+++ gallery/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'PASSWORD':'maurinesinami',
     }
     }
-}
+
 
 
 # Password validation
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Africa/Nairob'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
